8c8ed55a65f2f7ae5b8cd21e6905475a09938cfc
Vasiliy <sc-2005@yandex.ru>
2022-11-14 22:11:48 +0700

    Добавьте файлы проекта.

---
+++ GC-6/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace GC_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.holst = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // holst
+            // 
+            this.holst.AccumBits = ((byte)(0));
+            this.holst.AutoCheckErrors = false;
+            this.holst.AutoFinish = false;
+            this.holst.AutoMakeCurrent = true;
+            this.holst.AutoSwapBuffers = true;
+            this.holst.BackColor = System.Drawing.Color.Black;
+            this.holst.ColorBits = ((byte)(32));
+            this.holst.DepthBits = ((byte)(16));
+            this.holst.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.holst.Location = new System.Drawing.Point(0, 0);
+            this.holst.Name = "holst";
+            this.holst.Size = new System.Drawing.Size(634, 611);
+            this.holst.StencilBits = ((byte)(0));
+            this.holst.TabIndex = 0;
+            this.holst.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.holst.KeyDown += new System.Windows.Forms.KeyEventHandler(this.holst_KeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 611);
+            this.Controls.Add(this.holst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl holst;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GC-6/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+
+namespace GC_6
+{
+    public partial class Form1 : Form
+    {
+        //координаты кубика
+        readonly float[] cube = { 0,0,0, 0,1,0, 1,1,0, 1,0,0, 0,0,1, 0,1,1, 1,1,1, 1,0,1 };
+        //соединение точек кубика
+        readonly uint[] index = {0,1,2,3, 0,1,5,4, 4,5,6,7, 2,3,7,6, 1,2,6,5, 0,3,7,4, 1,5, 0,4, 3,7, 2,6 };
+        //readonly uint[] index = {1,0,2,3, 1,0,5,4, 5,4,6,7, 6,7,2,3, 1,2,5,6, 0,3,4,7};
+        //цвета для каждой точки
+        readonly float[] colors = {1,0,0.7f, 1,0,0, 1,0,0, 1,0.75f,0, 0,1,0, 0.5f,0,1, 0,1,0, 1,0.5f,1,};
+        //флаг запуска таймера 
+        bool flag = false;
+        public Form1()
+        {
+            InitializeComponent();
+            holst.InitializeContexts();
+            //прозрачность
+            //Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
+            //Gl.glEnable(Gl.GL_BLEND);
+
+            //буфер глубины для отсичения
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+
+            //начальное и конечное значение для осей
+            Gl.glOrtho(-2, 2, -2, 2, -2, 2);
+
+            Gl.glViewport(0, 0, holst.Width, holst.Height);
+            Gl.glClearColor(255, 255, 255, 1);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Draw();
+            holst.Invalidate();
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+
+        private void Draw()
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glPushMatrix();
+            Gl.glEnableClientState(Gl.GL_VERTEX_ARRAY); //используем массив вершин
+            //Gl.glEnableClientState(Gl.GL_COLOR_ARRAY);
+
+            Gl.glVertexPointer(3, Gl.GL_FLOAT, 0, cube); //в качестве массива вершин используем
+            Gl.glColorPointer(3, Gl.GL_FLOAT, 0, colors);
+            Gl.glTranslated(-0.5, -0.5, 0);
+            // массив вершин строится по индексам
+            Gl.glColor3f(0.8f, 0.8f, 0.8f);
+            Gl.glDrawArrays(Gl.GL_QUADS, 0, 4);
+            Gl.glColor3f(1, 1, 1);
+            Gl.glDrawElements(Gl.GL_QUADS, 24, Gl.GL_UNSIGNED_INT, index);
+            Gl.glColor3f(0, 0, 0);
+            Gl.glLineWidth(4);
+            Gl.glDrawElements(Gl.GL_LINES, 32, Gl.GL_UNSIGNED_INT, index);
+
+
+            Gl.glPopMatrix();
+            Gl.glDisableClientState(Gl.GL_VERTEX_ARRAY);
+            //Gl.glDisableClientState(Gl.GL_COLOR_ARRAY);
+
+            holst.Invalidate();
+        }
+
+        private void Rotate(float x, float y, float z)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glRotatef(2, x, y, z);
+            Draw();
+        }
+
+        private void holst_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+            if (e.KeyCode == Keys.Q)
+            {
+                Rotate(1f, 0, 0);
+            }
+            if (e.KeyCode == Keys.E)
+            {
+                Rotate(-1f, 0, 0);
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                Rotate(0, 1f, 0);
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                Rotate(0, -1f, 0);
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                Rotate(0, 0, 1f);
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                Rotate(0, 0, -1f);
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                if (flag == true)
+                {
+                    flag = false;
+                    timer1.Stop();
+                }
+                else if (flag == false)
+                {
+                    flag = true;
+                    timer1.Start();
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Rotate(1f,1f,1f);
+        }
+    }
+}
